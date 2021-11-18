ba69bfd20bdadf82cbf2111dba5109c0e7d20055
Serg Chernoivanenko <LonelyKeter@gmail.com>
2021-11-18 11:59:47 +0200

    __

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
-use serde::{Serialize, Deserialize, de::DeserializeOwned};
 use async_trait::*;
-#[macro_use] extern crate lazy_static;
+use lazy_static::lazy_static;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use thiserror::Error;
 
 pub trait LoginData {
     type Claim: Serialize + Clone;
     type Context;
     type Err;
 
-    fn to_claim(self, context: &Self::Context) 
-        -> Result<Self::Claim, Self::Err>;
+    fn to_claim(self, context: &Self::Context) -> Result<Self::Claim, Self::Err>;
 }
 
 #[async_trait]
@@ -17,14 +17,13 @@ pub trait LoginDataAsync {
     type Context;
     type Err;
 
-    async fn to_claim(self, context: &Self::Context) 
-        -> Result<Self::Claim, Self::Err>;
+    async fn to_claim(self, context: &Self::Context) -> Result<Self::Claim, Self::Err>;
 }
 
 #[derive(Serialize, Clone, Deserialize)]
 pub struct Payload<T: Clone> {
     claim: T,
-    exp: usize
+    exp: usize,
 }
 
 impl<T: Serialize + Clone + DeserializeOwned> Payload<T> {
@@ -45,94 +44,106 @@ pub trait Authenticator {
     fn check(data: Self::Data) -> Result<Self::Output, Self::Err>;
 }
 
-use chrono::{Duration};
-use std::marker::PhantomData;
+use chrono::Duration;
+use std::{default, marker::PhantomData};
 
 #[derive(Clone)]
 pub struct TokenSource<TClaim: Clone + Serialize + DeserializeOwned> {
     secret: Box<[u8]>,
     expiration_period: Duration,
-    _claim_type: PhantomData<TClaim>
+    _claim_type: PhantomData<TClaim>,
 }
 
 lazy_static! {
     pub static ref DEFAULT_EXPIRATION_PERIOD: Duration = Duration::days(1);
 }
 
-impl<TClaim: Clone + Serialize + DeserializeOwned> TokenSource<TClaim> {
-    pub fn deafult() -> Self {
-        let default = b"Some very cool secret";
+pub static DEFAULT_SECRET: &[u8] = b"Some very cool secret";
 
-        Self::from_secret(default)
+impl<TClaim: Clone + Serialize + DeserializeOwned> Default for TokenSource<TClaim> {
+    fn default() -> Self {
+        Self::new(DEFAULT_SECRET, *DEFAULT_EXPIRATION_PERIOD)
     }
+}
 
-    #[inline(always)]
-    pub fn from_secret(secret: &[u8]) -> Self {
+impl<TClaim: Clone + Serialize + DeserializeOwned> TokenSource<TClaim> {
+    pub fn new(secret: &[u8], expiration_period: Duration) -> Self {
         let secret = {
             let mut buff = Vec::with_capacity(secret.len());
             buff.extend_from_slice(secret);
             buff
-        };
+        }
+        .into_boxed_slice();
 
         Self {
-            secret: secret.into_boxed_slice(),
-            expiration_period: *DEFAULT_EXPIRATION_PERIOD,
-            _claim_type: PhantomData
+            secret,
+            expiration_period,
+            _claim_type: PhantomData,
         }
     }
 
-    pub fn create_token<T: LoginData<Claim = TClaim>>(&self, data: T, context: &T::Context) -> Result<String, Error> {
-        use jsonwebtoken::{encode, Algorithm, Header, EncodingKey};
+    #[inline(always)]
+    pub fn from_secret(secret: &[u8]) -> Self {
+        Self::new(secret, *DEFAULT_EXPIRATION_PERIOD)
+    }
+
+    #[inline(always)]
+    pub fn from_expiration_period(expiration_period: Duration) -> Self {
+        Self::new(DEFAULT_SECRET, expiration_period)
+    }
+
+    pub fn create_token<T: LoginData<Claim = TClaim>>(
+        &self,
+        data: T,
+        context: &T::Context,
+    ) -> Result<String, T::Err> {
+        use jsonwebtoken::{encode, Algorithm, EncodingKey, Header};
 
         let exp = calc_expiration_timestamp(self.expiration_period);
         let payload = Payload::<T::Claim> {
-            claim: data.to_claim(context).map_err(|_| Error::ClaimCreationError)?,
-            exp: exp as usize
+            claim: data.to_claim(context)?,
+            exp: exp as usize,
         };
 
         let header = Header::new(Algorithm::HS256);
 
-        let token = encode(&header, &payload, &EncodingKey::from_secret(&self.secret))?;
+        //Unmatching algorythm families in header and key will lead to panic
+        let token = encode(&header, &payload, &EncodingKey::from_secret(&self.secret)).unwrap();
 
         Ok(token)
     }
 
-    pub async fn create_token_async<T: LoginDataAsync<Claim = TClaim>>(&self, data: T, context: &T::Context) -> Result<String, Error> {
-        use jsonwebtoken::{encode, Algorithm, Header, EncodingKey};
+    //TODO: Change error kind to <T as LoginDataAsync>::Error
+    pub async fn create_token_async<T: LoginDataAsync<Claim = TClaim>>(
+        &self,
+        data: T,
+        context: &T::Context,
+    ) -> Result<String, T::Err> {
+        use jsonwebtoken::{encode, Algorithm, EncodingKey, Header};
 
         let exp = calc_expiration_timestamp(self.expiration_period);
         let payload = Payload::<T::Claim> {
-            claim: data.to_claim(context).await.map_err(|_| Error::ClaimCreationError)?,
-            exp: exp as usize
+            claim: data.to_claim(context).await?,
+            exp: exp as usize,
         };
 
         let header = Header::new(Algorithm::HS256);
 
-        let token = encode(&header, &payload, &EncodingKey::from_secret(&self.secret))?;
+        //Unmatching algorythm families in header and key will lead to panic
+        let token = encode(&header, &payload, &EncodingKey::from_secret(&self.secret)).unwrap();
 
         Ok(token)
     }
 
-    pub fn verify_token(&self, token: String) -> Result<TClaim, Error> {
-        use jsonwebtoken::{decode, DecodingKey, Algorithm, Validation};
+    pub fn verify_token(&self, token: &str) -> Result<TClaim, ValidationError> {
+        use jsonwebtoken::{decode, Algorithm, DecodingKey, Validation};
 
         let decoded = decode::<Payload<TClaim>>(
-            &token, 
-            &DecodingKey::from_secret(&self.secret), 
-            &Validation::new(Algorithm::HS256))
-            .map_err(|_| Error::JWTDecodingError)?;
-
-        Ok(decoded.claims.claim)
-    }
-
-    pub fn verify_token_str(&self, token: &str) -> Result<TClaim, Error> {
-        use jsonwebtoken::{decode, DecodingKey, Algorithm, Validation};
-
-        let decoded = decode::<Payload<TClaim>>(
-            token, 
-            &DecodingKey::from_secret(&self.secret), 
-            &Validation::new(Algorithm::HS256))
-            .map_err(|_| Error::JWTDecodingError)?;
+            token,
+            &DecodingKey::from_secret(&self.secret),
+            &Validation::new(Algorithm::HS256),
+        )
+        .map_err(ValidationError::on_verify)?;
 
         Ok(decoded.claims.claim)
     }
@@ -145,19 +156,25 @@ fn calc_expiration_timestamp(expiration_period: Duration) -> i64 {
         .timestamp()
 }
 
-unsafe impl<TClaim: Clone + Serialize + DeserializeOwned> Send for TokenSource<TClaim> { }
+unsafe impl<TClaim: Clone + Serialize + DeserializeOwned> Send for TokenSource<TClaim> {}
 
 #[derive(Debug)]
-pub enum Error {
-    ClaimCreationError,
-    JWTCreationError { inner: jsonwebtoken::errors::Error },
-    JWTDecodingError
+pub enum ValidationError {
+    InvalidAlgorithm,    
+    InvalidToken,
+    InvalidSignature,
+    FailedVerification
 }
 
-impl From<jsonwebtoken::errors::Error> for Error {
-    fn from(inner: jsonwebtoken::errors::Error) -> Self {
-        Self::JWTCreationError {
-            inner
+impl ValidationError {
+    fn on_verify(err: jsonwebtoken::errors::Error) -> Self {
+        match err.into_kind() {
+            jsonwebtoken::errors::ErrorKind::InvalidAlgorithm => Self::InvalidAlgorithm,
+            jsonwebtoken::errors::ErrorKind::InvalidToken => Self::InvalidToken,
+            jsonwebtoken::errors::ErrorKind::InvalidSignature |  
+            jsonwebtoken::errors::ErrorKind::ExpiredSignature |
+            jsonwebtoken::errors::ErrorKind::ImmatureSignature => Self::InvalidSignature,
+            _ => Self::FailedVerification
         }
     }
 }
